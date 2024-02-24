c2232e4d69dc7ce534881a3f4766fe0aa606d19f
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-24 19:36:11 +0300

    do height and weight values and calulate BMI

--- MyBMIcalculatorApp/Controller/ViewController.swift
+++ MyBMIcalculatorApp/Controller/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var heightSlider: UISlider!
+    @IBOutlet var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButtonPress(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = height / pow(weight, 2)
+        
+        print(bmi)
+    }
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let value = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(value) m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let value = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(value) kg"
+    }
+    
 }
 
